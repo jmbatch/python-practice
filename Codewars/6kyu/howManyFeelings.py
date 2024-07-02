a08494e5165c63c18c07df8bc13ef7bb23380a16
jmbatch <104248608+jmbatch@users.noreply.github.com>
2024-07-02 06:44:26 -0400

    Update howManyFeelings.py

--- Codewars/6kyu/howManyFeelings.py
+++ Codewars/6kyu/howManyFeelings.py
@@ -1,18 +1,18 @@
 
-#You have two arguments: string - a string of random letters(only lowercase) and array - an array of strings(feelings). Your task is to return how many specific feelings are in the array.
+# You have two arguments: string - a string of random letters(only lowercase) and array - an array of strings(feelings). Your task is to return how many specific feelings are in the array.
 #
-#For example:
-#string -> 'yliausoenvjw'
-#array -> ['anger', 'awe', 'joy', 'love', 'grief']
-#output -> '3 feelings.' // 'awe', 'joy', 'love'
+# For example:
+# string -> 'yliausoenvjw'
+# array -> ['anger', 'awe', 'joy', 'love', 'grief']
+# output -> '3 feelings.' // 'awe', 'joy', 'love'
 #
-#string -> 'griefgriefgrief'
-#array -> ['anger', 'awe', 'joy', 'love', 'grief']
-#output -> '1 feeling.' // 'grief'
-
-#string -> 'abcdkasdfvkadf'
-#array -> ['desire', 'joy', 'shame', 'longing', 'fear']
-#output -> '0 feelings.'
+# string -> 'griefgriefgrief'
+# array -> ['anger', 'awe', 'joy', 'love', 'grief']
+# output -> '1 feeling.' // 'grief'
+#
+# string -> 'abcdkasdfvkadf'
+# array -> ['desire', 'joy', 'shame', 'longing', 'fear']
+# output -> '0 feelings.'
 
 def count_feelings(st, arr):
     zero = 0
